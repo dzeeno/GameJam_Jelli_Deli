adf704938c270cc628bef121abe18c32d0f8ea5a
DarthMurky <joshjvv96@gmail.com>
2019-09-22 14:08:53 +0200

    dfgdg

---
+++ jellydelly/Assets/Beat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    public float BeatSpeed = 5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.Translate(BeatSpeed * Time.deltaTime, 0f, 0f);
+
+    }
+    private void OnCollisionEnter(Collision Other)
+    {
+        if (Other.collider.tag == "Player")
+        {
+            Destroy(this.gameObject, 0f);
+        }
+    }
+}
