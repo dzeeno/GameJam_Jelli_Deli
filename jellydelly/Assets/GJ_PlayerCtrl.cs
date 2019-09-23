24d14046b08e4c72d212c1296776e0999b7c6315
DarthMurky <joshjvv96@gmail.com>
2019-09-23 10:59:49 +0200

    pls work

--- jellydelly/Assets/GJ_PlayerCtrl.cs
+++ jellydelly/Assets/GJ_PlayerCtrl.cs
@@ -1,14 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GJ_PlayerCtrl : MonoBehaviour
 {
 	public GameObject Player ;
-    public Text ScoreTxt;
-    public GameObject ScoreObj;
-    public int Score = 0;
     //public float smoothing = 2.0f;
 
     //private Vector2 M_look;
@@ -24,15 +20,12 @@ public class GJ_PlayerCtrl : MonoBehaviour
     {
         Player = this.gameObject ;
         Cursor.visible = false ;
-        ScoreObj = GameObject.Find("Scoretext");
-        ScoreTxt = ScoreObj.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        P_Move () ;
-        ScoreTxt.text = "SCORE: " + Score;
+        P_Move () ;  
     }
 
     private void P_Move()
